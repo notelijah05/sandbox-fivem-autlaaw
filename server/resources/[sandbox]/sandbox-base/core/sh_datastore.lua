c07d8fc6187ca987115f9d48d6b7dc2be2132988
AutLaaw <Haakonrb1@gmail.com>
2025-09-27 17:39:28 +0200

    Datastore with typescript compatibility
    In typescript:
    
    Client side:
    
    const cash = exports["sandbox-base"].GetCharacterData("Cash");
    Server side:
    
      const SID = exports["sandbox-base"].GetCharacterData("SID", source);
      const firstName = exports["sandbox-base"].GetCharacterData("First", source);
      const lastName = exports["sandbox-base"].GetCharacterData("Last", source);

--- server/resources/[sandbox]/sandbox-base/core/sh_datastore.lua
+++ server/resources/[sandbox]/sandbox-base/core/sh_datastore.lua
@@ -42,6 +42,105 @@ exports("CreateStore", function(owner, key, data)
 	}
 end)
 
+exports("GetStore", function(owner, key)
+	if _stores[owner] and _stores[owner][key] then
+		local internalStore = {
+			Owner = owner,
+			Key = key,
+			SetData = function(self, var, data)
+				_stores[self.Owner][self.Key][var] = data
+
+				if self.Key == "Character" and IsDuplicityVersion() and not _blacklistedCharEvents[var] then
+					TriggerClientEvent("Characters:Client:SetData", _stores[self.Owner][self.Key]["Source"], var, data)
+				end
+			end,
+			GetData = function(self, var)
+				if var ~= nil and var ~= "" then
+					if
+						_stores[self.Owner]
+						and _stores[self.Owner][self.Key]
+						and _stores[self.Owner][self.Key][var] ~= nil
+					then
+						return _stores[self.Owner][self.Key][var]
+					else
+						return nil
+					end
+				else
+					return _stores[self.Owner][self.Key]
+				end
+			end,
+			DeleteStore = function(self)
+				exports["sandbox-base"]:DeleteStore(self.Owner, self.Key)
+			end,
+		}
+
+		local store = {
+			Owner = owner,
+			Key = key,
+			SetData = function(var, data)
+				if type(var) ~= "string" then
+					return nil
+				end
+				return internalStore:SetData(var, data)
+			end,
+			GetData = function(var, data)
+				local key = var
+				if type(var) ~= "string" and data and type(data) == "string" then
+					key = data
+				elseif type(var) ~= "string" then
+					return nil
+				end
+				return internalStore:GetData(key)
+			end,
+			DeleteStore = function()
+				return internalStore:DeleteStore()
+			end,
+		}
+
+		setmetatable(store, {
+			__index = function(self, key)
+				if type(self[key]) == "function" then
+					return self[key]
+				end
+				return self:GetData(key)
+			end,
+			__newindex = function(self, key, value)
+				self:SetData(key, value)
+			end
+		})
+
+		return store
+	end
+	return nil
+end)
+
+exports("GetCharacterData", function(key, source)
+	local playerId = source or (LocalPlayer and LocalPlayer.state.Character and LocalPlayer.state.Character.Owner)
+	local character = exports["sandbox-base"]:GetStore(playerId, "Character")
+	if character then
+		return character:GetData(key)
+	end
+	return nil
+end)
+
+exports("SetCharacterData", function(key, value, source)
+	local playerId = source or (LocalPlayer and LocalPlayer.state.Character and LocalPlayer.state.Character.Owner)
+	local character = exports["sandbox-base"]:GetStore(playerId, "Character")
+	if character then
+		return character:SetData(key, value)
+	end
+	return nil
+end)
+
+exports("DeleteCharacterStore", function(source)
+	local playerId = source or (LocalPlayer and LocalPlayer.state.Character and LocalPlayer.state.Character.Owner)
+	local character = exports["sandbox-base"]:GetStore(playerId, "Character")
+	if character then
+		return character:DeleteStore()
+	end
+	return nil
+end)
+
 exports("DeleteStore", function(owner, key)
 	if _stores[owner] then
 		_stores[owner][key] = nil
