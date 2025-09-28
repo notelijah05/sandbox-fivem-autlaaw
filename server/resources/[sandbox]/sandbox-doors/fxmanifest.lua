60326477d7a43ce598487126528e1c16a390c267
AutLaaw <Haakonrb1@gmail.com>
2025-09-28 19:54:09 +0200

    Update fxmanifest.lua

--- server/resources/[sandbox]/sandbox-doors/fxmanifest.lua
+++ server/resources/[sandbox]/sandbox-doors/fxmanifest.lua
@@ -5,18 +5,16 @@ client_script("@sandbox-base/exports/cl_error.lua")
 client_script("@sandbox-pwnzor/client/check.lua")
 
 client_scripts({
-	"config.lua",
-	"utils.lua",
 	"client/*.lua",
 })
 
 server_scripts({
-	"config.lua",
-	"utils.lua",
 	"server/*.lua",
 })
 
 shared_scripts({
+	"config.lua",
+	"utils.lua",
 	"shared/elevatorConfig.lua",
 	"shared/doorConfig/**/*.lua",
 })
