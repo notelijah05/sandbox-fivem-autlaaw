5bbe70cc7473b5ad218ec96cc7af23b54ddeb111
AutLaaw <Haakonrb1@gmail.com>
2025-10-12 16:32:53 +0200

    Remove prints

--- server/resources/[sandbox]/sandbox-robbery/server/signs/main.lua
+++ server/resources/[sandbox]/sandbox-robbery/server/signs/main.lua
@@ -50,9 +50,7 @@ AddEventHandler("Robbery:Server:Setup", function()
 			Player(source).state.SignRobbery = true
 			objects[#objects + 1] = { coords = data.coords, model = data.model }
 			TriggerClientEvent("Robbery:Client:DeleteSign", -1, data)
-			print("before item")
 			exports.ox_inventory:AddItem(char:GetData("SID"), data.item, 1, {}, 1)
-			print("after item")
 			exports['sandbox-characters']:RepAdd(source, "SignRobbery", math.random(35, 75))
 			Player(source).state.SignRobbery = false
 			cb(true)
