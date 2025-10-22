4bd49399ca621dc7d014d54ecead10f64fdecfe9
AutLaaw <Haakonrb1@gmail.com>
2025-10-22 02:30:48 +0200

    Add "keyfob" item to ox_inventory

--- server/resources/[ox]/ox_inventory/data/items.lua
+++ server/resources/[ox]/ox_inventory/data/items.lua
@@ -8190,4 +8190,15 @@ return {
     ['garbage'] = {
         label = 'Garbage',
     },
+    ['keyfob'] = {
+        label = 'Key Fob',
+        weight = 50,
+        stack = false,
+        close = true,
+        description = 'Keyfobs to open doors remotely.',
+        consume = 0,
+        client = {
+            export = 'ox_doorlock.useKeyfob'
+        }
+    },
 }
