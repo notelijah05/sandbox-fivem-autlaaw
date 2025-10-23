81c04c7bc8826fa72470df44ef10680059c7d003
AutLaaw <Haakonrb1@gmail.com>
2025-10-24 01:23:24 +0200

    (ox_inv) Update background image URL

--- server/resources/[ox]/ox_inventory/web/src/main.tsx
+++ server/resources/[ox]/ox_inventory/web/src/main.tsx
@@ -13,8 +13,7 @@ const root = document.getElementById('root');
 
 if (isEnvBrowser()) {
   // https://i.imgur.com/iPTAdYV.png - Night time img
-  root!.style.backgroundImage =
-    'url("https://cdn.discordapp.com/attachments/1422968097903612005/1427385558694498415/Screenshot_2025-10-10_221329.png?ex=68eeabe4&is=68ed5a64&hm=e8a715c7350b68e1e815e92dd49dc11cd5298efcb2dc0afaec6ada8ffda72c01&")';
+  root!.style.backgroundImage = 'url("https://i.imgur.com/Rro9l86.png")';
   root!.style.backgroundSize = 'cover';
   root!.style.backgroundRepeat = 'no-repeat';
   root!.style.backgroundPosition = 'center';
