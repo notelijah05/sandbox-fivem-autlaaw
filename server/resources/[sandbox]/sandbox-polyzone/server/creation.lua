b2d5a25f73c09ee102de9686873ae9bfd4108b5b
xMaddMackx <82854182+xMaddMackx@users.noreply.github.com>
2025-10-23 13:04:49 -0400

    Refactor zone creation and output handling
    Refactor zone printing functions to use SaveResourceFile for output. Introduce ConfigFormatEnabled to toggle output format.

--- server/resources/[sandbox]/sandbox-polyzone/server/creation.lua
+++ server/resources/[sandbox]/sandbox-polyzone/server/creation.lua
@@ -1,3 +1,5 @@
+local ConfigFormatEnabled = false 
+
 AddEventHandler('onResourceStart', function(resource)
 	if resource == GetCurrentResourceName() then
 		Wait(1000)
@@ -58,45 +60,33 @@ RegisterNetEvent("polyzone:printPoly")
 AddEventHandler("polyzone:printPoly", function(zone)
 	local src = source
 	local player = exports['sandbox-base']:FetchSource(src)
-	if not player.Permissions:IsAdmin() then
-		return
-	end
+	if not player.Permissions:IsAdmin() then return end
 
-	file = io.open("polyzone_created_zones.txt", "a")
-	io.output(file)
-	local output = parsePoly(zone)
-	io.write(output)
-	io.close(file)
+	local created_zones = LoadResourceFile(GetCurrentResourceName(), "polyzone_created_zones.txt") or ""
+	local output = created_zones .. parsePoly(zone)
+	SaveResourceFile(GetCurrentResourceName(), "polyzone_created_zones.txt", output, -1)
 end)
 
 RegisterNetEvent("polyzone:printCircle")
 AddEventHandler("polyzone:printCircle", function(zone)
 	local src = source
 	local player = exports['sandbox-base']:FetchSource(src)
-	if not player.Permissions:IsAdmin() then
-		return
-	end
+	if not player.Permissions:IsAdmin() then return end
 
-	file = io.open("polyzone_created_zones.txt", "a")
-	io.output(file)
-	local output = parseCircle(zone)
-	io.write(output)
-	io.close(file)
+	local created_zones = LoadResourceFile(GetCurrentResourceName(), "polyzone_created_zones.txt") or ""
+	local output = created_zones .. parseCircle(zone)
+	SaveResourceFile(GetCurrentResourceName(), "polyzone_created_zones.txt", output, -1)
 end)
 
 RegisterNetEvent("polyzone:printBox")
 AddEventHandler("polyzone:printBox", function(zone)
 	local src = source
 	local player = exports['sandbox-base']:FetchSource(src)
-	if not player.Permissions:IsAdmin() then
-		return
-	end
+	if not player.Permissions:IsAdmin() then return end
 
-	file = io.open("polyzone_created_zones.txt", "a")
-	io.output(file)
-	local output = parseBox(zone)
-	io.write(output)
-	io.close(file)
+	local created_zones = LoadResourceFile(GetCurrentResourceName(), "polyzone_created_zones.txt") or ""
+	local output = created_zones .. parseBox(zone)
+	SaveResourceFile(GetCurrentResourceName(), "polyzone_created_zones.txt", output, -1)
 end)
 
 function round(num, numDecimalPlaces)
@@ -109,78 +99,96 @@ function printoutHeader(name)
 end
 
 function parsePoly(zone)
-	local printout = printoutHeader(zone.name)
-	printout = printout .. "PolyZone:Create({\n"
-	for i = 1, #zone.points do
-		if i ~= #zone.points then
-			printout = printout
-				.. "  vector2("
-				.. tostring(zone.points[i].x)
-				.. ", "
-				.. tostring(zone.points[i].y)
-				.. "),\n"
-		else
-			printout = printout
-				.. "  vector2("
-				.. tostring(zone.points[i].x)
-				.. ", "
-				.. tostring(zone.points[i].y)
-				.. ")\n"
+	if ConfigFormatEnabled then
+		local printout = printoutHeader(zone.name)
+		printout = printout .. "points = {\n"
+
+		for i = 1, #zone.points do
+			if i ~= #zone.points then
+				printout = printout .. "  vector2(" .. tostring(zone.points[i].x) .. ", " .. tostring(zone.points[i].y) .."),\n"
+			else
+				printout = printout .. "  vector2(" .. tostring(zone.points[i].x) .. ", " .. tostring(zone.points[i].y) ..")\n"
+			end
 		end
+
+		printout = printout .. "},\nname = \"" .. zone.name .. "\",\n--minZ = " .. zone.minZ .. ",\n--maxZ = " .. zone.maxZ .. ",\n--debugPoly = true\n\n"
+		
+		return printout
+	else
+		local printout = printoutHeader(zone.name)
+		printout = printout .. "PolyZone:Create({\n"
+		for i = 1, #zone.points do
+			if i ~= #zone.points then
+				printout = printout .. "  vector2(" .. tostring(zone.points[i].x) .. ", " .. tostring(zone.points[i].y) .."),\n"
+			else
+				printout = printout .. "  vector2(" .. tostring(zone.points[i].x) .. ", " .. tostring(zone.points[i].y) ..")\n"
+			end
+		end
+
+		printout = printout .. "}, {\n  name = \"" .. zone.name .. "\",\n  --minZ = " .. zone.minZ .. ",\n  --maxZ = " .. zone.maxZ .. "\n})\n\n"
+		
+		return printout
 	end
-	printout = printout
-		.. '}, {\n  name="'
-		.. zone.name
-		.. '",\n  --minZ = '
-		.. zone.minZ
-		.. ",\n  --maxZ = "
-		.. zone.maxZ
-		.. "\n})\n\n"
-	return printout
 end
 
 function parseCircle(zone)
-	local printout = printoutHeader(zone.name)
-	printout = printout .. "CircleZone:Create("
-	printout = printout
-		.. "vector3("
-		.. tostring(round(zone.center.x, 2))
-		.. ", "
-		.. tostring(round(zone.center.y, 2))
-		.. ", "
-		.. tostring(round(zone.center.z, 2))
-		.. "), "
-	printout = printout .. tostring(zone.radius) .. ", "
-	printout = printout
-		.. '{\n  name="'
-		.. zone.name
-		.. '",\n  useZ='
-		.. tostring(zone.useZ)
-		.. ",\n  --debugPoly=true\n})\n\n"
-	return printout
+	if ConfigFormatEnabled then
+		local printout = printoutHeader(zone.name)
+		printout = printout .. "coords = "
+		printout = printout .. "vector3(" .. tostring(round(zone.center.x, 2)) .. ", " .. tostring(round(zone.center.y, 2))  .. ", " .. tostring(round(zone.center.z, 2)) .."),\n"
+		printout = printout .. "radius = " .. tostring(zone.radius) .. ",\n"
+		printout = printout .. "name = \"" .. zone.name .. "\",\nuseZ = " .. tostring(zone.useZ) .. ",\n--debugPoly = true\n\n"
+
+		return printout
+	else
+		local printout = printoutHeader(zone.name)
+		printout = printout .. "CircleZone:Create("
+		printout = printout .. "vector3(" .. tostring(round(zone.center.x, 2)) .. ", " .. tostring(round(zone.center.y, 2))  .. ", " .. tostring(round(zone.center.z, 2)) .."), "
+		printout = printout .. tostring(zone.radius) .. ", "
+		printout = printout .. "{\n  name = \"" .. zone.name .. "\",\n  useZ = " .. tostring(zone.useZ) .. ",\n  --debugPoly = true\n})\n\n"
+
+		return printout
+	end
 end
 
 function parseBox(zone)
-	local printout = printoutHeader(zone.name)
-	printout = printout .. "BoxZone:Create("
-	printout = printout
-		.. "vector3("
-		.. tostring(round(zone.center.x, 2))
-		.. ", "
-		.. tostring(round(zone.center.y, 2))
-		.. ", "
-		.. tostring(round(zone.center.z, 2))
-		.. "), "
-	printout = printout .. tostring(zone.length) .. ", "
-	printout = printout .. tostring(zone.width) .. ", "
+  	if ConfigFormatEnabled then
+		local printout = printoutHeader(zone.name)
+		printout = printout .. "coords = "
+		printout = printout .. "vector3(" .. tostring(round(zone.center.x, 2)) .. ", " .. tostring(round(zone.center.y, 2))  .. ", " .. tostring(round(zone.center.z, 2)) .."),\n"
+		printout = printout .. "length = " .. tostring(zone.length) .. ",\n"
+		printout = printout .. "width = " .. tostring(zone.width) .. ",\n"
+		printout = printout .. "name = \"" .. zone.name .. "\",\nheading = " .. zone.heading .. ",\n--debugPoly = true"
 
-	printout = printout .. '{\n  name = "' .. zone.name .. '",\n  heading = ' .. zone.heading .. ",\n  --debugPoly=true"
-	if zone.minZ then
-		printout = printout .. ",\n  minZ = " .. tostring(round(zone.minZ, 2))
-	end
-	if zone.maxZ then
-		printout = printout .. ",\n  maxZ = " .. tostring(round(zone.maxZ, 2))
-	end
-	printout = printout .. "\n})\n\n"
-	return printout
+		if zone.minZ then
+			printout = printout .. ",\nminZ = " .. tostring(round(zone.minZ, 2))
+		end
+
+		if zone.maxZ then
+			printout = printout .. ",\nmaxZ = " .. tostring(round(zone.maxZ, 2))
+		end
+
+    	printout = printout .. "\n\n"
+
+    	return printout
+  	else
+		local printout = printoutHeader(zone.name)
+		printout = printout .. "BoxZone:Create("
+		printout = printout .. "vector3(" .. tostring(round(zone.center.x, 2)) .. ", " .. tostring(round(zone.center.y, 2))  .. ", " .. tostring(round(zone.center.z, 2)) .."), "
+		printout = printout .. tostring(zone.length) .. ", "
+		printout = printout .. tostring(zone.width) .. ", "
+		printout = printout .. "{\n  name = \"" .. zone.name .. "\",\n  heading = " .. zone.heading .. ",\n  --debugPoly = true"
+
+		if zone.minZ then
+			printout = printout .. ",\n  minZ = " .. tostring(round(zone.minZ, 2))
+		end
+
+		if zone.maxZ then
+			printout = printout .. ",\n  maxZ = " .. tostring(round(zone.maxZ, 2))
+		end
+
+		printout = printout .. "\n})\n\n"
+
+		return printout
+  	end
 end
