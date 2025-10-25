343f9dd76fe730678ec9bd35a951ffc5a3268839
Elijah <128943965+notelijah05@users.noreply.github.com>
2025-10-24 22:58:10 -0400

    Modify PoliceCars and EMSCars configuration
    Updated PoliceCars and EMSCars to use test spawn code.

--- server/resources/[sandbox]/sandbox-police/server/config.lua
+++ server/resources/[sandbox]/sandbox-police/server/config.lua
@@ -8,21 +8,10 @@ Config = {
 		{ item = "handcuffs",       count = 1 },
 		{ item = "fluffyhandcuffs", count = 1 },
 	},
-	PoliceCars = {
-		[`nkballer7`] = true,
-		[`nkcaracara2`] = true,
-		[`nkdominator3`] = true,
-		[`nkgranger2`] = true,
-		[`nkstx`] = true,
-		[`nktenf`] = true,
-		[`nktraining`] = true,
-		[`nkvigero2`] = true,
-		[`nkvstr`] = true,
-		[`policebretro`] = true,
-		[`bcat`] = true,
+	PoliceCars = { -- below is used to have secured compartments inside of PD vehicles - where it says test-spawn-code-here is where you put the spawn code for a car and so on below, just copy and paste - same thing for EMS vehicles below
+		[`test-spawn-code-here`] = true,
 	},
 	EMSCars = {
-		[`emsa`] = true,
-		[`emsnspeedo`] = true,
+		[`test-spawn-code-here`] = true,
 	},
 }
