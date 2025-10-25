3a1bb0a972357c0380f4769dbd4751d202588c95
Elijah <128943965+notelijah05@users.noreply.github.com>
2025-10-24 22:50:24 -0400

    Remove description from fxmanifest.lua
    Removed the description line from fxmanifest.lua.

--- server/resources/[sandbox]/sandbox-weapons/fxmanifest.lua
+++ server/resources/[sandbox]/sandbox-weapons/fxmanifest.lua
@@ -1,7 +1,6 @@
 game("gta5")
 lua54("yes")
 fx_version("cerulean")
-description("Stop Panda Complaining Like a Bitch")
 
 client_script("@sandbox-base/exports/cl_error.lua")
 client_script("@sandbox-pwnzor/client/check.lua")
